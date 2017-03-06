c25d5d31e43361dda1f882053350a536321a3259
Ivania Donoso <ivania.donoso@gmail.com>
2017-03-06 00:07:26 -0300

    Cambios en vistas

--- static/js/load_select.js
+++ static/js/load_select.js
@@ -1,35 +1,4 @@
-server = "http://localhost:5000/";
-
-function formTemplate(product, num_components) {
-    return "<div class=\"panel panel-default\"> " +
-        "<div class=\"panel-heading\" role=\"tab\" id=\"panel_" + num_components + "\">" +
-            "<h4 class=\"panel-title\"> " +
-                "<a id=\"panel_title_" + num_components + "\"role=\"button\" data-toggle=\"collapse\" data-parent=\"#components_list\" href=\"#body_" + num_components + "\" ></a>" +
-            "</h4>" +
-        "</div>" +
-        "<div id=\"body_" + num_components + "\" class=\"panel-collapse collapse in\" role=\"tabpanel\" >" +
-            "<div class=\"panel-body\">" +
-                "<form id=\"component_" + num_components + "\" class=\"form-horizontal\" method=\"POST\" action=\"/add/component/" + product + "\" onsubmit=\"addComponent(this, " + num_components + "); return false;\">"
-                + "<div class=\"form-group\">"
-                    + "<label for=\"product_in_" + num_components + "\">Producto</label>"
-                    + "<select id=\"product_in_" + num_components + "\" class=\"form-control\" onchange='loadUnit()'></select>" +
-                "</div>"
-                + "<div class=\"form-group\">"
-                    + "<input type=\"number\" id=\"quantity_" + num_components + "\" class=\"form-control\" placeholder=\"Cantidad\">"
-                    + "<label id=\"label_quantity_" + num_components + "\" for=\"quantity\"></label>" +
-                "</div>"
-            + "<div class=\"form-group\">"
-                    + "<input type=\"number\" id=\"comment_" + num_components + "\" class=\"form-control\" placeholder=\"comment\">"
-                    + "<label id=\"label_comment_" + num_components + "\" for=\"comment\"></label>" +
-                "</div>"
-                + "<input id=\"form_btn_"+ num_components+ "\"type=\"submit\" class=\"btn btn-primary\" value=\"Agregar\">" +
-            "</form>"
-            + "</div>" +
-        "</div>";
-
-}
-
-function addComponent(oFormElement, number_component, product_name) {
+function addComponent(oFormElement) {
     var xhr = new XMLHttpRequest();
     xhr.onload = function () {
         alert(xhr.responseText);
@@ -43,27 +12,10 @@ function addComponent(oFormElement, number_component, product_name) {
     xhr.send("product_in=" + product_in + "&quantity=" + quantity + "&comment=" + comment);
     xhr.onreadystatechange = function () {
         if (this.readyState == 2 && this.status == 201) {
-            document.getElementById("body_" + number_component).classList.toggle("in");
-            document.getElementById("panel_title_" + number_component ).innerHTML = document.getElementById("product_in").name;
-            document.getElementById("form_btn_" + number_component).classList.remove("btn-primary");
-            document.getElementById("form_btn_" + number_component).classList.add("btn-success");
-            num_components += 1;
+            console.log("Success")
         }
     };
     return false;
-}
-
-function loadForm(div, product) {
-    if (document.getElementById("component_" + num_components)) {
-        document.getElementById("add_component").classList.remove("btn-primary")
-        document.getElementById("add_component").classList.add("btn-danger")
-    } else {
-        document.getElementById(div).innerHTML += formTemplate(product, num_components);
-        loadProducts();
-        document.getElementById("add_component").classList.add("btn-primary")
-        document.getElementById("add_component").classList.remove("btn-danger")
-
-    }
 }
 
 
@@ -74,20 +26,15 @@ function loadUnit() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("label_quantity").innerHTML = this.responseText;
+            document.getElementById("add_on_product_in").innerHTML = this.responseText;
         }
     };
     xhttp.open("GET", "/product/get_unit/" + product_id, true);
     xhttp.send();
 }
 
-function loadProducts() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("product_in").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "/product/get_all_choices", true);
-    xhttp.send();
+
+function getSelectedValue(selection_combo){
+    var e = document.getElementById(selection_combo);
+    return [e.options[e.selectedIndex].value, e.options[e.selectedIndex].text]
 }
